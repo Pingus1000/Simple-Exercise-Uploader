ace57dd28884789db1eb296f75d1b3815940ffd4
Pingus1000 <49938550+Pingus1000@users.noreply.github.com>
2020-05-30 11:08:14 +0200

    Update reset.php

--- schüler/reset.php
+++ schüler/reset.php
@@ -1,5 +1,5 @@
 <?php
-$programmname = "megz";
+$programmname = file_get_contents('../config/name.txt');
 $domain = file_get_contents('../config/domain.txt');
 setcookie("klasse", "", time() - 3600);
 header("Location: http://$domain/$programmname/schüler/"); 
